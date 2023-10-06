45a2e5928c6e74e44ca95bab1121a39f5f61d4f4
yo-404 <yogeshy2801@gmail.com>
2023-10-07 02:52:20 +0530

    First Commit

---
+++ src/assets/index.js
@@ -0,0 +1,117 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import back from "./back.png";
+import webdev from "./webdev.png";
+import devops from "./devops.png";
+import opensource from "./opensource.png"
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import go from "./tech/go.png";
+import bootstrap from "./tech/bootstrap.png";
+import aws from "./tech/aws.png";
+import argocd from "./tech/argocd.png";
+import ansible from "./tech/ansible.png";
+import k8s from "./tech/k8s.png";
+import terraform from "./tech/terraform.png";
+import jenkins from "./tech/jenkins.png";
+import python from "./tech/python.png";
+import cpp from "./tech/cpp.png";
+import mysql from "./tech/mysql.png";
+import bash from "./tech/bash.png";
+import ubuntu from "./tech/ubuntu.png";
+import gitactions from "./tech/gitactions.png";
+
+
+import cognizant from "./workex/cognizant.png";
+import wordpress from "./workex/wordpress.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import autocicd from "./autocicd.png";
+import myfitness from "./myfitness.png"
+import beetune from "./beetune.png";
+import cyber from "./cyber.png";
+import hospital from "./hospital.png";
+import packers from "./packers.png";
+import valeries from "./valeries.png";
+import musicrnn from "./musicrnn.png";
+import tunelives from "./tunelives.png";
+import cinevault from "./cinevault.png";
+import cicd from "./cicd.png";
+import bookingprx from "./bookingprx.png";
+
+
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwind,
+  typescript,
+  carrent,
+  jobit,
+  tripguide,
+  cognizant,
+  wordpress,
+  go,
+  bootstrap,
+  aws,
+  argocd,
+  ansible,
+  k8s,
+  terraform,
+  jenkins,
+  python,
+  cpp,
+  mysql,
+  bash,
+  ubuntu,
+  gitactions,
+  autocicd,
+  myfitness,
+  beetune,
+  cyber,
+  hospital,
+  packers,
+  valeries,
+  musicrnn,
+  tunelives,
+  cinevault,
+  cicd,
+  bookingprx,
+  back,
+  webdev,
+  devops,
+  opensource,
+};
